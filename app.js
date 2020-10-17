565c48a26d4bb9e7348dc38c05fe8f216b03e516
rickjp <3741326+RickJP@users.noreply.github.com>
2020-10-17 23:40:24 +0900

    ADD: a lot of basic setup for server, db, handlebars, routes & passport auth

---
+++ app.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
+const connectDB = require('./config/db');
+
+// Load config file
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Sessions
+app.use(
+  session({
+    secret: 'dshfjksk67868HJK',
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+);
