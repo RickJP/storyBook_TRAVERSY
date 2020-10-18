a8ad0941ab78e2568c3675c25e705205d2da225f
rickjp <3741326+RickJP@users.noreply.github.com>
2020-10-18 11:20:41 +0900

    ADD: ensureAuth, ensureGuest middleware; gs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
 
 // Load config file
@@ -31,6 +33,7 @@ app.use(
     secret: 'dshfjksk67868HJK',
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   }),
 );
 
--- routes/auth.js
+++ routes/auth.js
@@ -20,4 +20,11 @@ router.get(
   },
 );
 
+// @desc Logout user
+// @route /auth/logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
