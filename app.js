25f84dd28e657c28ab36a0f711fde1860dfc9738
rickjp <3741326+RickJP@users.noreply.github.com>
2020-10-18 15:50:18 +0900

    ADD: edit story, methodOverride for delete

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -23,18 +24,40 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies & delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  }),
+);
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 // Handlebars Helpers
-const { formatDate } = require('./helpers/hbs');
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs');
 
 // Handlebars
 app.engine(
   '.hbs',
-  exphbs({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs' }),
+  exphbs({
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  }),
 );
 app.set('view engine', '.hbs');
 
@@ -51,6 +74,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')));
