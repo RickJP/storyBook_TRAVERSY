4a30bc7cb762d9483306a46dd10370990c111366
rickjp <3741326+RickJP@users.noreply.github.com>
2020-10-18 11:59:29 +0900

    ADD: add stories, display in dashboard

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ connectDB();
 
 const app = express();
 
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -47,6 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 3000;
 
